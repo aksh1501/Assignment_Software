768426d50d1c982f989e1f435fbc18df2220d933
aksh1501 <akshitsharma1501@gmail.com>
2024-02-15 18:23:04 +0530

    DigitalLibrary

---
+++ Assignment_Software/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Net.Mail;
+using Shapes;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        // Ans 1 Drawing shapes using Compite Design Pattern 
+        IElement line1 =new Line(new Point(2,1),new Point(5,1),10);
+
+        IElement line2=new Line(new Point(5,1),new Point(3,3),5);
+
+        IElement line3=new Line(new Point(2,1),new Point(3,3),6);
+
+        List <IElement> lst= new List<IElement> ();
+
+        lst.Add(line1);
+
+        lst.Add(line2);
+
+        lst.Add(line3);
+
+        IElement triangle = new Polygon(lst);
+
+        triangle.draw();
+
+
+        //Ans 2 Ability to Sort Edge weights
+
+        triangle.getSortedWeights(new MergeSort());
+
+        Console.WriteLine();
+
+        // Decorate the lines
+
+        IElement line4 =new Line(new Point(7,8),new Point(10,11),11);
+
+        IElement line5=new Line(new Point(10,11),new Point(12,8),5);
+
+        IElement line6=new Line(new Point(7,8),new Point(12,8),2);
+
+        line4=new ArrowedElement(line4);
+        
+        line5=new OvaledElement(line5);
+
+        line6= new OvaledElement(line6);
+
+        IElement tr2=new Polygon([line4,line5,line6]);
+
+        tr2.draw();
+
+
+        // Ans 4 and 5 Creating  an abstract factory of points anf Lines. Each of the abstract factory is a Singleton. Similaly for Polygons it is possible.
+        
+        IElementFactory ef=RhombP_DashedL_Factory.getFactoryInstance();
+        
+        IElementFactory ef1=CircP_BoldL_Factory.getFactoryInstance();
+
+        IElement point8=ef.getPoint(6,9);
+        IElement point9=ef1.getPoint(5,5);
+        IElement point10=ef1.getPoint(2,1);
+
+        IElement line7=ef.getLine(point8,point9,14);
+       
+        //point8.draw();
+        
+        IElement line8=ef1.getLine(point9,point10,34);
+
+        IElement line9=ef.getLine(point10,point8,3);
+
+        IElement Tr5=new Polygon([line7,line8,line8]);
+
+        Tr5.draw(); 
+    
+    
+        //Ans 5 
+    
+    }
+
+}
---
+++ Assignment_Software/Shapes.cs
@@ -0,0 +1,450 @@
+using System.Reflection;
+using System;
+using System.Security.Principal;
+
+namespace Shapes
+{
+    interface IElement
+    {
+        void draw();
+
+        int getWeight();
+
+        void getSortedWeights(sortAlgorithm s);
+    }
+
+    class Point: IElement
+    {
+        private int x,y;
+        private int weight=0;
+
+        public Point(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        virtual public void draw()
+        {
+            Console.Write(" Point (" + x + " , " + y + ") ");
+        }
+
+
+        public void getSortedWeights(sortAlgorithm s)
+        {
+            Console.WriteLine();
+        }
+
+        public int getWeight()
+        {
+            return 0;
+        }
+    }
+
+    class Line : IElement
+    {
+
+        IElement end1;
+        IElement end2;
+
+        private int weight;
+
+        public Line( IElement s, IElement e, int w)
+        {
+            end1=s;
+            end2=e;
+            weight=w;
+        }
+
+        virtual public void draw()
+        {
+            Console.Write("A normal line joining :  ");
+            end1.draw();
+            Console.Write(", ");
+            end2.draw();
+            Console.WriteLine(" with weight " + weight);
+        }
+
+        public int getWeight()
+        {
+            return weight;
+        }
+
+        public void getSortedWeights(sortAlgorithm s)
+        {
+            Console.WriteLine("Weight is" + weight);
+        }
+    }
+
+    class Polygon : IElement
+    {
+
+        List <IElement> components;
+
+        public Polygon(List <IElement> components)
+        {
+            this.components=components;
+        }
+
+        public void draw()
+        {
+            Console.WriteLine("Drawing a shape with :  ");
+
+            foreach(IElement e in components)
+            {
+                e.draw();
+            }
+
+            Console.WriteLine();
+        }
+
+        public int getWeight()
+        {
+            int totalWeight=0;
+
+            foreach(Line e in components)
+            {
+                totalWeight+=e.getWeight();
+            }
+
+            return totalWeight;
+        }
+
+        public void getSortedWeights(sortAlgorithm algo)
+        {
+            Sorter s=new Sorter(algo);
+
+            List <int> weightList=new List<int> ();
+
+            foreach(Line e in components)
+            {
+                weightList.Add(e.getWeight());
+            }
+
+            s.Sort(weightList);
+
+            Console.Write("List of Sorted Weights : ");
+
+            foreach(int x in weightList)
+            {
+                Console.Write(" " + x + " ,");
+            }
+
+            Console.WriteLine();
+        }
+
+    }
+
+    class Sorter
+    {
+        private sortAlgorithm sortAlgo;
+
+        public Sorter(sortAlgorithm algorithm)
+        {
+            sortAlgo=algorithm;
+        }
+
+        public void Sort(List <int> l)
+        {
+            sortAlgo.sort(l);
+        }  
+    }
+
+    interface sortAlgorithm
+    {
+        void sort(List <int> l);
+    }
+
+
+
+    class MergeSort : sortAlgorithm
+    {
+        public void sort(List<int> list)
+        {
+            if (list.Count <= 1)
+                return;
+
+            int middle = list.Count / 2;
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+
+            for (int i = 0; i < middle; i++)
+            {
+                left.Add(list[i]);
+            }
+            for (int i = middle; i < list.Count; i++)
+            {
+                right.Add(list[i]);
+            }
+
+            sort(left);
+            sort(right);
+            merge(left, right, list);
+        }
+
+        private void merge(List<int> left, List<int> right, List<int> merged)
+        {
+            int leftIndex = 0, rightIndex = 0, mergedIndex = 0;
+
+            while (leftIndex < left.Count && rightIndex < right.Count)
+            {
+                if (left[leftIndex] <= right[rightIndex])
+                {
+                    merged[mergedIndex] = left[leftIndex];
+                    leftIndex++;
+                }
+                else
+                {
+                    merged[mergedIndex] = right[rightIndex];
+                    rightIndex++;
+                }
+                mergedIndex++;
+            }
+
+            while (leftIndex < left.Count)
+            {
+                merged[mergedIndex] = left[leftIndex];
+                leftIndex++;
+                mergedIndex++;
+            }
+
+            while (rightIndex < right.Count)
+            {
+                merged[mergedIndex] = right[rightIndex];
+                rightIndex++;
+                mergedIndex++;
+            }
+        }
+    }
+
+
+    class QuickSort : sortAlgorithm
+    {
+        public void sort(List <int> l)
+        {
+            QuickSortRecursive(l, 0, l.Count - 1);
+        }
+
+        private void QuickSortRecursive(List<int> list, int left, int right)
+        {
+            if (left < right)
+            {
+                int partitionIndex = Partition(list, left, right);
+
+                QuickSortRecursive(list, left, partitionIndex - 1);
+                QuickSortRecursive(list, partitionIndex + 1, right);
+            }
+        }      
+
+        private int Partition(List<int> list, int left, int right)
+        {
+            int pivot = list[right];
+            int i = left - 1;
+
+            for (int j = left; j < right; j++)
+            {
+                if (list[j] <= pivot)
+                {
+                    i++;
+
+                    int temp = list[i];
+                    list[i] = list[j];
+                    list[j] = temp;
+                }
+            }
+
+            int temp1 = list[i + 1];
+            list[i + 1] = list[right];
+            list[right] = temp1;
+
+            return i + 1;
+        }
+    }
+
+
+    abstract class ElementWrapper : IElement
+    {
+        IElement WrappedElement;
+
+        public ElementWrapper(IElement e) 
+        {
+            WrappedElement = e;
+        }
+
+        virtual public void draw()
+        {
+            WrappedElement.draw();
+        }
+
+        public void getSortedWeights(sortAlgorithm s)
+        {
+            WrappedElement.getSortedWeights(s);
+        }
+
+        public int getWeight()
+        {
+            return WrappedElement.getWeight();
+        }
+    }
+
+    class ArrowedElement : ElementWrapper
+    {
+        public ArrowedElement(IElement e) : base(e)
+        {
+        }
+
+        override public void draw()
+        {
+            base.draw();
+            AddArrows();
+        }
+
+        void AddArrows()
+        {
+            Console.WriteLine("Added Arrows to the Line.");
+            Console.WriteLine();
+        }
+    }
+
+
+   class OvaledElement : ElementWrapper
+    {
+        public OvaledElement(IElement e) : base(e)
+        {
+        }
+
+        override public void draw()
+        {
+            base.draw();
+            AddOvals();
+        }
+
+        void AddOvals()
+        {
+            Console.WriteLine("Added Ovals to the line.");
+            Console.WriteLine();
+        }
+    }
+
+    class DashedLine : Line
+    {
+        public DashedLine(IElement s, IElement e, int w) : base(s, e, w)
+        {
+        }
+
+        new public void draw()
+        {
+            base.draw();
+            Console.Write(" [This is a dashed Line.] ");
+        }
+    }
+
+
+    class BoldLine : Line
+    {
+        public BoldLine(IElement s, IElement e, int w) : base(s, e, w)
+        {
+        }
+
+        public void draw()
+        {
+            base.draw();
+            Console.Write(" [This is a super bold line.] ");
+        }
+    }
+
+
+    class RhombicPoint : Point
+    {
+        public RhombicPoint(int x, int y) : base(x, y)
+        {
+        }
+
+        override public void draw()
+        {
+            base.draw();
+            Console.Write(" [This is a Rhombic Point.] ");
+        }
+    }
+
+
+     class CircledPoint : Point
+    {
+        public CircledPoint(int x, int y) : base(x, y)
+        {
+        }
+
+        override public void draw()
+        {
+            base.draw();
+            Console.Write(" [This is a Circled Point.] ");
+        }
+    }
+
+
+    interface IElementFactory
+    {
+        IElement getPoint(int x,int y);
+        IElement getLine(IElement s, IElement e, int w);
+    }
+
+
+
+    class CircP_BoldL_Factory : IElementFactory
+    {
+        static CircP_BoldL_Factory instance = null;
+
+        private CircP_BoldL_Factory()
+        {
+
+        }
+
+        static public  CircP_BoldL_Factory getFactoryInstance()
+        {
+            if(instance==null)
+            {
+                instance=new CircP_BoldL_Factory();
+            }
+
+            return instance;
+        }
+        public IElement getLine(IElement s, IElement e, int w)
+        {
+            return new BoldLine(s,e,w);
+        }
+
+        public IElement getPoint(int x,int y)
+        {
+           return new CircledPoint(x,y);
+        }
+    }
+
+    class RhombP_DashedL_Factory : IElementFactory
+    {
+        static RhombP_DashedL_Factory instance=null;
+
+        private RhombP_DashedL_Factory(){}
+
+        static public RhombP_DashedL_Factory getFactoryInstance()
+        {
+            if(instance==null)
+            {
+                instance=new RhombP_DashedL_Factory();
+            }
+
+            return instance;
+        }
+
+        public IElement getLine(IElement s, IElement e, int w)
+        {
+            return new DashedLine(s,e,w);
+        }
+
+        public IElement getPoint(int x, int y)
+        {
+            return new RhombicPoint(x,y);
+        }
+    }
+
+
+}
